b554518e0d91c872291bebda78cdd691ce13fadd
Raim Delgado <raim223@seoultech.ac.kr>
2020-04-29 13:51:35 +0900

    add source files - prototype

---
+++ start.sh
@@ -0,0 +1,9 @@
+## This is a project made within Seoul National University of Science and Technology 
+## Embedded Systems Laboratory 2018 - Raimarius Tolentino Delgado 
+
+## Start-up for dynamically linked executable file 
+
+
+
+export LD_LIBRARY_PATH=/usr/xenomai/lib 
+./bin/test_perf $1 $2 $3 $4 $5 $6
