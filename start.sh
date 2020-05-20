0f061893d92fcee083785ab45a1b17d89cb2a294
Raim Delgado <raim223@seoultech.ac.kr>
2020-05-20 11:00:55 +0900

    add mutexes_2

---
+++ start.sh
@@ -0,0 +1,8 @@
+## This is a project made within Seoul National University of Science and Technology 
+## Embedded Systems Laboratory 2018 - Raimarius Tolentino Delgado 
+
+## Start-up for dynamically linked executable file 
+
+
+
+./bin/test_perf $1 $2 $3 $4 $5 $6
